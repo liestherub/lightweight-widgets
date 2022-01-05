ab3fad94185e3c267d61bce37cf5dfc5b3b18195
liestherub <liestherub1@gmail.com>
2022-01-05 23:42:28 +0800

    upgrade storybook config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,18 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
+  stories: [
+    "../src/**/*.stories.mdx", 
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
-}
+    "@storybook/addon-essentials",
+    {
+      name: "@storybook/addon-postcss",
+      options: {
+        postcssLoaderOptions: {
+          implementation: require("postcss"),
+        },
+      },
+    },
+  ],
+};
