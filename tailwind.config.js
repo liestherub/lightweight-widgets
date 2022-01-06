67d1b08ce4d81a5c7c73d2f8e9581134ac7ff528
liestherub1 <liestherub1@gmail.com>
2022-01-06 19:48:12 +0800

    style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/client/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      ...colors,
+      primary: colors.cyan,
+    },
     cursor: {
       "col-resize": "col-resize",
     }
