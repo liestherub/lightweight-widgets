a71be7cab0d42dfdbaa8574e3a51d56b80cd8c6a
liestherub <liestherub1@gmail.com>
2022-04-20 15:17:00 +0800

    toast

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,20 @@ module.exports = {
       cursor: {
         "col-resize": "col-resize",
       },
+      animation: {
+        enter: "enter .2s ease-out",
+        leave: "leave .15s ease-in forwards",
+      },
+      keyframes: {
+        enter: {
+          from: { transform: "scale(.9)", opacity: 0 },
+          to: { transform: "scale(1)", opacity: 1 },
+        },
+        leave: {
+          from: { transform: "scale(1)", opacity: 1 },
+          to: { transform: "scale(.9)", opacity: 0 },
+        },
+      },
     },
   },
   plugins: [require("@tailwindcss/typography")],
